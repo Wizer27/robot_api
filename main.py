a0f01d2526df2f93b0c8c368fc0573aedf4cd7f0
Wizer27 <svd2yar@gmail.com>
2025-11-25 18:24:32 +0300

    safe get

--- main.py
+++ main.py
@@ -6,8 +6,24 @@ import json
 import time
 import requests
 
+secrets_path = "data/secrets.json"
+
+def get_api_key() -> str:
+    try:
+        with open(secrets_path,"r") as file:
+            data = json.load(file)
+        return data["api"]    
+    except Exception as e:
+        raise KeyError("No such key")
+
 
 app = FastAPI()
 @app.get("/")
 async def main():
     return "MIRA-API"
+
+async def safe_get(req:Request):
+    api = req.headers.get("X-API-KEY")
+    if not api or not hmac.compare_digest(api,get_api_key()):
+        raise HTTPException(status_code = 401,detail = "Invalid api key")
+
