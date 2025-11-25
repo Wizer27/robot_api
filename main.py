3bd05c3f927ebf509ff630477ba0a4f85e9b4017
Wizer27 <svd2yar@gmail.com>
2025-11-25 22:04:47 +0300

    ?

--- main.py
+++ main.py
@@ -16,14 +16,34 @@ def get_api_key() -> str:
     except Exception as e:
         raise KeyError("No such key")
 
-
+def get_signature_key() -> str:
+    try:
+        with open(secrets_path,"r") as file:
+            data = json.load(file)
+        return data["signature"]    
+    except Exception as e:
+        raise KeyError("No such key")
+    
 app = FastAPI()
 @app.get("/")
 async def main():
     return "MIRA-API"
-
+#-----SECURITY-----
 async def safe_get(req:Request):
     api = req.headers.get("X-API-KEY")
     if not api or not hmac.compare_digest(api,get_api_key()):
         raise HTTPException(status_code = 401,detail = "Invalid api key")
+    
 
+def verify_signature(data:dict,signature:str,timestamp:str) -> bool:
+    if int(time.time) - int(timestamp) > 300:
+        return False
+    KEY = get_signature_key()
+    data_to_verify = data.copy()
+    data_to_verify.pop("signature",None)
+
+    data_str = json.dumps(data_to_verify, sort_keys=True, separators=(',', ':'))
+    expected_signature = hmac.new(KEY.encode(), data_str.encode(), hashlib.sha256).hexdigest()
+    return hmac.compare_digest(expected_signature,signature)
+#-----SECURITY-----
+    
